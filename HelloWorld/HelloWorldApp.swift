f7491c46bcd33c50a30ade0f091c094505ec0fc7
sharuban <s.s.ruban98@gmail.com>
2024-11-19 20:59:07 +0530

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Sharuban Sharu on 11/19/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
